5350d81e2ae9daa4fff311a9f935e59b09200f89
Brahian Julian Mejia Perez <brayantqm123@gmail.com>
2019-09-07 10:10:41 -0500

    Subida del programa de nombre.

---
+++ NombrePersonalizado/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace NombrePersonalizado
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnir = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 149);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite el nombre:";
+            // 
+            // btnir
+            // 
+            this.btnir.Location = new System.Drawing.Point(476, 144);
+            this.btnir.Name = "btnir";
+            this.btnir.Size = new System.Drawing.Size(75, 23);
+            this.btnir.TabIndex = 1;
+            this.btnir.Text = "Ir";
+            this.btnir.UseVisualStyleBackColor = true;
+            this.btnir.Click += new System.EventHandler(this.btnir_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(370, 146);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblnombre.Location = new System.Drawing.Point(398, 217);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(35, 13);
+            this.lblnombre.TabIndex = 3;
+            this.lblnombre.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btnir);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnir;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label lblnombre;
+    }
+}
+
---
+++ NombrePersonalizado/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NombrePersonalizado
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblnombre.Visible = false;
+        }
+
+        private void btnir_Click(object sender, EventArgs e)
+        {
+            string nombre = txtnombre.Text;
+            lblnombre.Text = nombre;
+            lblnombre.Font = new System.Drawing.Font(label1.Font, FontStyle.Bold);
+            lblnombre.ForeColor = Color.Red;
+            lblnombre.BorderStyle = BorderStyle.FixedSingle;
+            lblnombre.Visible = true;
+        }
+    }
+}
